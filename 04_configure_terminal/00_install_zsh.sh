35732b0bcc50e6a48b63500a742cdca70abd1380
matchaeggtart <matchaeggtart@hotmail.com>
2022-06-23 06:48:30 +0800

    update

--- 04_configure_terminal/00_install_zsh.sh
+++ 04_configure_terminal/00_install_zsh.sh
@@ -3,4 +3,4 @@ sudo dnf install zsh -y
 
 # sudo dnf config-manager --add-repo https://download.opensuse.org/repositories/shells:zsh-users:zsh-syntax-highlighting/Fedora_Rawhide/shells:zsh-users:zsh-syntax-highlighting.repo -y
 # sudo dnf install zsh-syntax-highlighting -y
-# sudo reboot now
+sudo reboot now
