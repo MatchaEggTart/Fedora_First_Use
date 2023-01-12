b8f22dcdde1c2cf11940959f3a2994bf51e9bfd7
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-01-13 02:54:46 +0800

    update

--- 03_install_fonts/01_install_meslolgs.sh
+++ 03_install_fonts/01_install_meslolgs.sh
@@ -1,4 +1,6 @@
+cd ~/Downloads
+git clone https://github.com/kalaschnik/meslolgs-nf-template ~/Downloads/meslolgs-nf-template
 mkdir -p ~/.local/share/fonts/meslolgs
-cp ./meslolgs/* ~/.local/share/fonts/meslolgs
+cp ./meslolgs-nf-template/* ~/.local/share/fonts/meslolgs
 fc-cache -v 
 ls ~/.local/share/fonts/meslolgs/
--- 04_configure_terminal/10_install_zsh-syntax-highlighting.sh
+++ 04_configure_terminal/10_install_zsh-syntax-highlighting.sh
@@ -1,4 +1,4 @@
-git clone https://gitee.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Activate the plugin in ~/.zshcr
 # plugins=( [plugins...] zsh-syntax-highlighting)
