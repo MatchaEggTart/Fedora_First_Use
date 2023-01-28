0c1b23039dfa80de71ee6fce7edfc342e9cc27ff
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-01-29 03:20:34 +0800

    update

--- 04_configure_terminal/07_install_nord_theme.sh
+++ 04_configure_terminal/07_install_nord_theme.sh
@@ -1,3 +1,3 @@
-git clone --depth=1 https://gitee.com/matchaeggtart/nord-gnome-terminal.git ${HOME}/Downloads/nord-gnome-terminal
+git clone --depth=1 https://github.com/arcticicestudio/nord-gnome-terminal.git ${HOME}/Downloads/nord-gnome-terminal
 cd ${HOME}/Downloads/nord-gnome-terminal/src
 sh nord.sh
