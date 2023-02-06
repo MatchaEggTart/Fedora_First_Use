b0ec624880db50073dc2bf6e646e28fc3fcde4e7
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-02-06 14:45:56 +0800

    update

--- 07_install_development_tools/01_install_nodejs/02_configure_mirror_nodejs.sh
+++ 07_install_development_tools/01_install_nodejs/02_configure_mirror_nodejs.sh
@@ -1,6 +1,8 @@
 # npm install -g cnpm --registry=https://registry.npm.taobao.org
 # cnpm -v
-sudo npm install -g nrm  --registry=https://registry.npm.taobao.org
+# sudo npm install -g nrm  --registry=https://registry.npm.taobao.org
 # nrm ls
 # nrm use cnpm
+npm config set registry https://registry.npmmirror.com/
 
+npm config get registry
