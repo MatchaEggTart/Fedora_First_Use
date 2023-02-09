5e62877a322d508a9a3d3f903d4a86aeab9d9c62
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-02-09 17:11:50 +0800

    update

--- 03_install_fonts/01_install_meslolgs.sh
+++ 03_install_fonts/01_install_meslolgs.sh
@@ -1,7 +1,7 @@
 cd ~/Downloads
 git clone https://github.com/kalaschnik/meslolgs-nf-template ~/Downloads/meslolgs-nf-template
 mkdir -p ~/.local/share/fonts/meslolgs
-cp ./meslolgs-nf-template/* ~/.local/share/fonts/meslolgs
+cp -r ./meslolgs-nf-template/* ~/.local/share/fonts/meslolgs
 cd ~/.local/share/fonts/meslolgs
 fc-cache -v 
 ls ~/.local/share/fonts/meslolgs/
