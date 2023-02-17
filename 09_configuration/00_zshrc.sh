400a7263dbe51b992ef0e911499bc69efa9ac473
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-02-18 01:40:14 +0800

    update

---
+++ 09_configuration/00_zshrc.sh
@@ -0,0 +1,26 @@
+# Personnal config
+alias rm="rm -i"
+alias mv="mv -i"
+
+# Vim
+# alias vim='gvim -v'
+alias vim="vimx"
+alias nv="nvim"
+
+# open QQMusic
+alias qqmusic="qqmusic --no-sandbox"
+# close QQMusic
+alias killqqmusic="killall -9 qqmusic"
+
+# vterm
+vterm_printf() {
+    if [ -n "$TMUX" ] && ([ "${TERM%%-*}" = "tmux" ] || [ "${TERM%%-*}" = "screen" ]); then
+        # Tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+    elif [ "${TERM%%-*}" = "screen" ]; then
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$1"
+    else
+        printf "\e]%s\e\\" "$1"
+    fi
+}
---
+++ 09_configuration/01_bash_profile.sh
@@ -0,0 +1,3 @@
+# set QQMusic environment
+QQMUSIC_HOME="/opt/qqmusic"
+PAHT=$PATH:"$QQMUSIC_HOME/qqmusic"
