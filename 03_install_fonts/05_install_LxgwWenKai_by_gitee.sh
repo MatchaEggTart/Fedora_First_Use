d371beae40ec5382650f693d027670ae82aea863
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-02-28 01:43:59 +0800

    update

---
+++ 03_install_fonts/05_install_LxgwWenKai_by_gitee.sh
@@ -0,0 +1,11 @@
+cd ~/Downloads
+# svn checkout https://github.com/lxgw/LxgwWenKai/trunk/fonts/TTF
+git clone https://gitee.com/mirrors/lxgw-wenkai.git ~/Downloads/lxgw-wenkai
+# mv TTF lxgwwenkai
+mkdir -p ~/.local/share/fonts/lxgw-wenkai
+cp -r ~/Downloads/lxgw-wenkai/fonts/TTF/* ~/.local/share/fonts/lxgw-wenkai
+cd ~/.local/share/fonts/lxgw-wenkai
+fc-cache -v
+
+
+ls ~/.local/share/fonts/lxgw-wenkai
