5d20c296bf1a395b2775ace2b6adcf1ff031ce83
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-07-25 10:34:52 +0800

    update

--- 02_install_tools/01_install_edge.sh
+++ 02_install_tools/01_install_edge.sh
@@ -1,7 +1,9 @@
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
 sudo mv /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge.repo
-sudo dnf install microsoft-edge-stable -y
+# sudo dnf install microsoft-edge-stable -y
+# sudo dnf install microsoft-edge-dev -y
+sudo dnf install microsoft-edge-beta -y
 
 echo "REMEMBER CHANGE THE DEFAULT BROWER!!!!!!!!!!"
 echo "REMEMBER CHANGE THE DEFAULT BROWER!!!!!!!!!!"
