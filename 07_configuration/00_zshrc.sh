142467bbdfe6a2d4da5818e62c58f94a731d782a
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-08-20 19:03:40 +0800

    update

--- 07_configuration/00_zshrc.sh
+++ 07_configuration/00_zshrc.sh
@@ -18,7 +18,7 @@ alias nv="nvim"
 
 # Edge
 # alias edge='func() {microsoft-edge-stable "$1" --nohub;}; func'
-alias edge='func() {microsoft-edge-beta "$1" --nohub;}; func'
+alias edge="microsoft-edge-beta --nohub"
 # alias edge='func() {microsoft-edge-dev "$1" --nohub;}; func'
 
 # open QQMusic
@@ -28,6 +28,3 @@ alias killqqmusic="killall -9 qqmusic"
 
 # default editor
 export EDITOR=nvim
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
